7a08de0947f45655841d8c3dc490cf7e4c2e1f59
Da Li <daniel.lee.bit@gmail.com>
2015-04-30 00:15:27 -0700

    + Added more dimensions and metrics

--- cnpolitics_ga_report_v0.py
+++ cnpolitics_ga_report_v0.py
@@ -59,11 +59,30 @@ def main(argv):
 
   # Try to make a request to the API. Print the results or handle errors.
   try:
-    first_profile_id = get_first_profile_id(service)
-    if not first_profile_id:
+    cnpolitics_profile_id = get_cnpolitics_profile_id(service)
+    if not cnpolitics_profile_id:
       print 'Could not find a valid profile for this user.'
     else:
-      results = get_top_keywords(service, first_profile_id)
+
+      results = get_top_pageviews(service, cnpolitics_profile_id)
+      print_pageview_results(results)
+
+      results = get_top_continents(service, cnpolitics_profile_id)
+      print_results(results)
+
+      results = get_top_cities(service, cnpolitics_profile_id)
+      print_results(results)
+
+      results = get_top_browsers(service, cnpolitics_profile_id)
+      print_results(results)
+
+      results = get_top_os(service, cnpolitics_profile_id)
+      print_results(results)
+
+      results = get_top_devices(service, cnpolitics_profile_id)
+      print_results(results)
+
+      results = get_audience_info(service, cnpolitics_profile_id)
       print_results(results)
 
   except TypeError, error:
@@ -81,7 +100,7 @@ def main(argv):
            'the application to re-authorize')
 
 
-def get_first_profile_id(service):
+def get_cnpolitics_profile_id(service):
   """Traverses Management API to return the first profile id.
 
   This first queries the Accounts collection to get the first account ID.
@@ -100,15 +119,15 @@ def get_first_profile_id(service):
   accounts = service.management().accounts().list().execute()
 
   if accounts.get('items'):
-    firstAccountId = accounts.get('items')[1].get('id')       # get the nth item
+    cnpoliticsAccountId = accounts.get('items')[1].get('id')       # get the nth item
     webproperties = service.management().webproperties().list(
-        accountId=firstAccountId).execute()
+        accountId=cnpoliticsAccountId).execute()
 
     if webproperties.get('items'):
-      firstWebpropertyId = webproperties.get('items')[0].get('id')
+      cnpoliticsWebpropertyId = webproperties.get('items')[0].get('id')
       profiles = service.management().profiles().list(
-          accountId=firstAccountId,
-          webPropertyId=firstWebpropertyId).execute()
+          accountId=cnpoliticsAccountId,
+          webPropertyId=cnpoliticsWebpropertyId).execute()
 
       if profiles.get('items'):
         return profiles.get('items')[0].get('id')
@@ -131,15 +150,182 @@ def get_top_keywords(service, profile_id):
 
   return service.data().ga().get(
       ids='ga:' + profile_id,
-      start_date='2015-01-01',
-      end_date='2015-04-15',
-      metrics='ga:visits,ga:pageviews,ga:sessions',
+      start_date='2015-04-01',
+      end_date='2015-04-28',
+      metrics='ga:visits,ga:sessions',
       dimensions='ga:source,ga:keyword',
       sort='-ga:visits',
       filters='ga:medium==organic',
       start_index='1',
       max_results='25').execute()
 
+def get_top_pageviews(service, profile_id):
+  """Executes and returns data from the Core Reporting API.
+
+  This queries the API for the top 25 organic search terms by visits.
+
+  Args:
+    service: The service object built by the Google API Python client library.
+    profile_id: String The profile ID from which to retrieve analytics data.
+
+  Returns:
+    The response returned from the Core Reporting API.
+  """
+
+  return service.data().ga().get(
+      ids='ga:' + profile_id,
+      start_date='2015-04-22',
+      end_date='2015-04-28',
+      metrics='ga:pageviews,ga:avgTimeOnPage',
+      dimensions='ga:pageTitle',
+      sort='-ga:pageviews',
+      filters='ga:medium==organic',
+      start_index='1',
+      max_results='30').execute()
+
+def get_top_continents(service, profile_id):
+  """Executes and returns data from the Core Reporting API.
+
+  This queries the API for the top 25 organic search terms by visits.
+
+  Args:
+    service: The service object built by the Google API Python client library.
+    profile_id: String The profile ID from which to retrieve analytics data.
+
+  Returns:
+    The response returned from the Core Reporting API.
+  """
+
+  return service.data().ga().get(
+      ids='ga:' + profile_id,
+      start_date='2015-04-01',
+      end_date='2015-04-28',
+      metrics='ga:visits,ga:avgPageLoadTime,ga:avgServerResponseTime',
+      dimensions='ga:continent',
+      sort='-ga:visits',
+      filters='ga:medium==organic',
+      start_index='1',
+      max_results='7').execute()
+
+def get_top_cities(service, profile_id):
+  """Executes and returns data from the Core Reporting API.
+
+  This queries the API for the top 25 organic search terms by visits.
+
+  Args:
+    service: The service object built by the Google API Python client library.
+    profile_id: String The profile ID from which to retrieve analytics data.
+
+  Returns:
+    The response returned from the Core Reporting API.
+  """
+
+  return service.data().ga().get(
+      ids='ga:' + profile_id,
+      start_date='2015-04-01',
+      end_date='2015-04-28',
+      metrics='ga:visits,ga:avgPageLoadTime,ga:avgServerResponseTime',
+      dimensions='ga:city',
+      sort='-ga:visits',
+      filters='ga:medium==organic',
+      start_index='1',
+      max_results='10').execute()
+
+def get_top_browsers(service, profile_id):
+  """Executes and returns data from the Core Reporting API.
+
+  This queries the API for the top 25 organic search terms by visits.
+
+  Args:
+    service: The service object built by the Google API Python client library.
+    profile_id: String The profile ID from which to retrieve analytics data.
+
+  Returns:
+    The response returned from the Core Reporting API.
+  """
+
+  return service.data().ga().get(
+      ids='ga:' + profile_id,
+      start_date='2015-04-01',
+      end_date='2015-04-28',
+      metrics='ga:visits',
+      dimensions='ga:browser',
+      sort='-ga:visits',
+      filters='ga:medium==organic',
+      start_index='1',
+      max_results='5').execute()
+
+def get_top_os(service, profile_id):
+  """Executes and returns data from the Core Reporting API.
+
+  This queries the API for the top 25 organic search terms by visits.
+
+  Args:
+    service: The service object built by the Google API Python client library.
+    profile_id: String The profile ID from which to retrieve analytics data.
+
+  Returns:
+    The response returned from the Core Reporting API.
+  """
+
+  return service.data().ga().get(
+      ids='ga:' + profile_id,
+      start_date='2015-04-01',
+      end_date='2015-04-28',
+      metrics='ga:visits',
+      dimensions='ga:operatingSystem',
+      sort='-ga:visits',
+      filters='ga:medium==organic',
+      start_index='1',
+      max_results='5').execute()
+
+def get_top_devices(service, profile_id):
+  """Executes and returns data from the Core Reporting API.
+
+  This queries the API for the top 25 organic search terms by visits.
+
+  Args:
+    service: The service object built by the Google API Python client library.
+    profile_id: String The profile ID from which to retrieve analytics data.
+
+  Returns:
+    The response returned from the Core Reporting API.
+  """
+
+  return service.data().ga().get(
+      ids='ga:' + profile_id,
+      start_date='2015-04-01',
+      end_date='2015-04-28',
+      metrics='ga:visits',
+      dimensions='ga:mobileDeviceModel',
+      sort='-ga:visits',
+      filters='ga:medium==organic',
+      start_index='1',
+      max_results='10').execute()
+
+def get_audience_info(service, profile_id):
+  """Executes and returns data from the Core Reporting API.
+
+  This queries the API for the top 25 organic search terms by visits.
+
+  Args:
+    service: The service object built by the Google API Python client library.
+    profile_id: String The profile ID from which to retrieve analytics data.
+
+  Returns:
+    The response returned from the Core Reporting API.
+  """
+
+  return service.data().ga().get(
+      ids='ga:' + profile_id,
+      start_date='2015-04-01',
+      end_date='2015-04-28',
+      metrics='ga:visits,ga:pageviews,ga:avgTimeOnPage,a:avgTimeOnSite',
+      dimensions='ga:visitorGender',
+      sort='-ga:visits',
+      filters='ga:medium==organic',
+      start_index='1',
+      max_results='10').execute()
 
 def print_results(results):
   """Prints out the results.
@@ -172,6 +358,40 @@ def print_results(results):
   else:
     print 'No Rows Found'
 
+def print_pageview_results(results):
+  """Prints out the results.
+
+  This prints out the profile name, the column headers, and all the rows of
+  data.
+
+  Args:
+    results: The response returned from the Core Reporting API.
+  """
+
+  print
+  print 'Profile Name: %s' % results.get('profileInfo').get('profileName')
+  print
+
+  # Print header.
+  output = []
+  for header in results.get('columnHeaders'):
+    output.append('%30s' % header.get('name'))
+  print ''.join(output)
+
+  # Print data table.
+  if results.get('rows', []):
+    for row in results.get('rows'):
+      output = []
+      i = 0
+      for cell in row:
+        if i == 0:
+          cell = cell[:-18]
+        output.append('%30s' % cell)
+        i = i + 1
+      print ''.join(output)
+
+  else:
+    print 'No Rows Found'
 
 if __name__ == '__main__':
   main(sys.argv)
